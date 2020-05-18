44516a1fa06c0d8e259dc4a563a6ed361fa922ac
GangwarDinesh <dineshgngwr@gmail.com>
2020-05-18 16:24:55 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import BrandInfo from '../src/components/brandInfo/BrandInfo';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrandInfo />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/brandSearch/BrandSearch.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class BrandSearch extends Component{
+    render() {
+        return <div></div>;
+    }
+}
+
+export default BrandSearch;
