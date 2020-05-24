2f32b38ca8e5d886548fecc349f5eed1efaa031e
GangwarDinesh <dineshgngwr@gmail.com>
2020-05-24 22:36:50 +0530

    pagination

--- src/components/productDetails/ProductDetails.js
+++ src/components/productDetails/ProductDetails.js
@@ -1,35 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './productDetails.module.css';
 
-const productDetails = (props) => (
-    <article className={styles.ProductDetails}>
-        <div className="Info">
-            <div className={styles.ProductInfoSection}>
-                <p> 
-                    <span style={{fontWeight: 'bold'}}>Brand name : </span> {props.brandName}
-                </p>
-                <p> 
-                    <span style={{fontWeight: 'bold'}}>Category : </span>{props.category}
-                </p>
-                <p> 
-                    <span style={{fontWeight: 'bold'}}>Product based on : </span>
-                    <span>
-                        {
-                            props.country === "India" ? 
-                            <span style={{color : "green", fontWeight: "bold"}}>{props.country}</span> : 
-                            props.country
-                        }</span>
-                </p>
-                <p>
-                    {
-                        props.country === "India" && props.country !== null ? 
-                                <a href={props.link}>Click here to Buy</a> : "" 
-                    }
-                    
-                </p>
-            </div>
-        </div>
-    </article>
-);
-
-export default productDetails;
+class ProductDetails extends Component{
+    
+    render() {
+        let country = "";
+        let link = "";
+        if(this.props.country === "India"){
+            country = <span style={{color : "#008000", fontWeight: "bold"}}>{this.props.country}</span>;
+            link = <a href={this.props.link}>Click here to Buy</a>;
+        }else if(this.props.country === "China"){
+            country = <span style={{color : "#FF0000", fontWeight: "bold"}}>{this.props.country}</span>;
+        }else{
+            country = <span style={{color : "#FFA500", fontWeight: "bold"}}>{this.props.country}</span>;
+            link = <a href={this.props.link}>Click here to Buy</a>;
+        }
+        return (
+                <article className={styles.ProductDetails}>
+                        <div className="Info">
+                            <div className={styles.ProductInfoSection}>
+                                <p> 
+                                    <span style={{fontWeight: 'bold'}}>Brand name : </span> {this.props.brandName}
+                                </p>
+                                <p> 
+                                    <span style={{fontWeight: 'bold'}}>Category : </span>{this.props.category}
+                                </p>
+                                <p> 
+                                    <span style={{fontWeight: 'bold'}}>Product based on : </span>
+                                    <span>{country}</span>
+                                </p>
+                                <p>{link}</p>
+                            </div>
+                        </div>
+                    </article>
+        );
+    }
+}
+export default ProductDetails;
